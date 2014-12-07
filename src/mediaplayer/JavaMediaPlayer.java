7c39d1720e2b7135d5ce7b8d458e3801dae1328e
Daniel Better <danielbt@mail.mta.ac.il>
2014-12-07 22:06:28 +0200

    Added support for opening a new video file via MenuBar

--- src/mediaplayer/JavaMediaPlayer.java
+++ src/mediaplayer/JavaMediaPlayer.java
@@ -6,13 +6,17 @@
 
 package mediaplayer;
 
+import java.io.File;
+import java.net.MalformedURLException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 /**
@@ -21,12 +25,47 @@ import javafx.stage.Stage;
  */
 public class JavaMediaPlayer extends Application {
     
+    Player player;
+    FileChooser fileChooser = new FileChooser();
+    
     @Override
-    public void start(Stage primaryStage) {
+    public void start(final Stage primaryStage) {
      
         try {
-            Player player = new Player("file:///C:/TestVideo.mp4");
-            Scene scene = new Scene(player, 720, 510, Color.BLACK);
+            
+            /* The proper hierarchy between MenuItem, Menu and MenuBar is the following:
+                Each Menu, contais several MenuItems
+                One MenuBar, contains several Menu's 
+            */
+            MenuItem open = new MenuItem("Open");
+            Menu file = new Menu("File");
+            MenuBar menu = new MenuBar();
+                    
+            file.getItems().add(open);
+            menu.getMenus().add(file);
+            
+            open.setOnAction(new EventHandler<ActionEvent>() {
+
+                @Override
+                public void handle(ActionEvent t) {
+                    player.player.pause();
+                    File file = fileChooser.showOpenDialog(primaryStage);
+                    if (file != null){
+                        try{
+                            player = new Player(file.toURI().toURL().toExternalForm());
+                            Scene scene = new Scene(player, 720, 535, Color.BLACK);
+                            primaryStage.setScene(scene);
+                        } catch (MalformedURLException e){
+                            // handle later
+                        }
+                                    
+                    }
+                }
+            });
+            
+            player = new Player("file:///C:/TestVideo.mp4");
+            player.setTop(menu);
+            Scene scene = new Scene(player, 720, 535, Color.BLACK);
             primaryStage.setScene(scene);
             primaryStage.show();
         } catch (Exception e) {
